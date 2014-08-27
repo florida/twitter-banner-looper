adcb6d7dc60361d8926b5b19b46e507292e27d43
Florida Elago <florida.elago@gmail.com>
2014-08-26 21:12:32 -0700

    docs link and faces of bmo

--- app.rb
+++ app.rb
@@ -8,3 +8,7 @@ client = Twitter::REST::Client.new do |config|
   config.access_token        = ENV["YOUR_ACCESS_TOKEN"]
   config.access_token_secret = ENV["YOUR_ACCESS_SECRET"]
 end
+
+
+# yass I found it: http://rdoc.info/gems/twitter/Twitter/REST/Users#update_profile_banner-instance_method
+# https://landofooo.com/community/topic/2712-the-faces-of-bmo/
